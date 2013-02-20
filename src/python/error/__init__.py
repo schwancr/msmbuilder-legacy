7d251733ab623a40eebeb387f3870a69d18eda9d
Christian Schwantes <schwancr@stanford.edu>
2013-02-19 19:52:42 -0800

    msmbuilder.error.singhal now includes code that can correctly calculate eigenvalue error according to Nina's method for DENSE matrices. Somehow I got it to agree with ssaCalculator.py (Dan Ensign's implementation) on the first try! Not that I'm bragging or anything.

---
+++ src/python/error/__init__.py
@@ -0,0 +1,3 @@
+
+from baseclasses import MSMError
+from singhal import SinghalError
---
+++ src/python/error/baseclasses.py
@@ -0,0 +1,18 @@
+"""
+This sub-module of msmbuilder provides classes that can calculate errors
+for various quantities you may wish to calculate. For instance, several methods
+will allow for calculating error bars for the eigenvalues / eigenvectors of 
+a particular MSM.
+"""
+
+class MSMError(object):
+    """
+    Base class for calculating some error on an MSM.
+    """
+
+    # Need to put methods here when I figure out what belongs...
+
+class EigenvalueError(object):
+    """
+    This class is a base class for calculating error bars for the 
+    """
